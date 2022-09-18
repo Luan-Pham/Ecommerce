480c49525aef620aa89bd7c879f927193fd4fdee
unknown <phamexb@gmail.com>
2022-09-17 20:03:29 -0700

    fixed small issues with all route to final working state

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require("../config/connection");
+const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
@@ -16,15 +16,17 @@ ProductTag.init(
     product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "product",
-        key: "id",
+        model: 'product',
+        key: 'id',
+        unique: false,
       },
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "tag",
-        key: "id",
+        model: 'tag',
+        key: 'id',
+        unique: false,
       },
     },
   },
@@ -33,7 +35,7 @@ ProductTag.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "product_tag",
+    modelName: 'product_tag',
   }
 );
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,9 +34,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = await Category.create({
-      category_id: req.body.category_id,
-    });
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -62,7 +60,7 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+  // delete one category by its `id` value
   try {
     const categoryData = await Category.destroy({
       where: {
